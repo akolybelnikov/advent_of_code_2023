8f25f4a025adef7ac6ae6554ec252d66737d4c11
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-12-08 12:08:46 +0100

    day 8 part 1

--- src/bin/day08/main.rs
+++ src/bin/day08/main.rs
@@ -1,16 +1,12 @@
+use std::collections::HashMap;
+
 #[derive(Debug, PartialEq)]
 enum Instruction {
     Left,
     Right,
 }
 
-struct Node {
-    value: String,
-    left: String,
-    right: String,
-}
-
-fn parse_node(input: &str) -> Node {
+fn parse_node(input: &str) -> (String, Vec<String>) {
     let mut split = input.split(" = ");
     let value = split.next().map(|s| s.to_string()).unwrap_or_default();
 
@@ -24,11 +20,11 @@ fn parse_node(input: &str) -> Node {
 
     let right = children.next().unwrap_or_default().to_owned();
 
-    Node { value, left, right }
+    (value, vec![left, right])
 }
 
-fn parse_nodes(input: &Vec<String>) -> Vec<Node> {
-    input.iter().map(|s| parse_node(s)).collect()
+fn parse_nodes(input: &[String]) -> HashMap<String, Vec<String>> {
+    input.iter().map(|s| parse_node(s)).collect::<HashMap<_, _>>()
 }
 
 fn parse_instructions(input: &str) -> Vec<Instruction> {
@@ -42,8 +38,39 @@ fn parse_instructions(input: &str) -> Vec<Instruction> {
         .collect()
 }
 
+fn walk_tree(nodes: HashMap<String, Vec<String>>, instructions: &Vec<Instruction>) -> i64 {
+    let mut current_node = "AAA".to_string();
+    let mut steps = 0;
+    'outer: loop {
+        for instruction in instructions.iter().cycle() {
+            steps += 1;
+            match instruction {
+                Instruction::Left => {
+                    let next_node = nodes[&current_node][0].clone();
+                    current_node = next_node;
+                }
+                Instruction::Right => {
+                    let next_node = nodes[&current_node][1].clone();
+                    current_node = next_node;
+                }
+            }
+            if current_node == "ZZZ" {
+                break 'outer;
+            }
+        }
+    }
+    steps
+}
+
+fn part_1(filename: &str) -> i64 {
+    let lines = advent_of_code_2023::read_lines(filename).unwrap();
+    let nodes = parse_nodes(&lines[2..]);
+    let instructions = parse_instructions(&lines[0]);
+    walk_tree(nodes, &instructions)
+}
+
 fn main() {
-    println!("Hello from day8!");
+    println!("Part 1: {}", part_1("src/bin/day08/input.txt"));
 }
 
 #[cfg(test)]
@@ -54,9 +81,9 @@ mod tests {
     fn test_parse_node() {
         let input = "AAA = (BBB, CCC)";
         let node = parse_node(input);
-        assert_eq!(node.value, "AAA");
-        assert_eq!(node.left, "BBB");
-        assert_eq!(node.right, "CCC");
+        assert_eq!(node.0, "AAA");
+        assert_eq!(node.1[0], "BBB");
+        assert_eq!(node.1[1], "CCC");
     }
 
     #[test]
@@ -68,15 +95,12 @@ mod tests {
         ];
         let nodes = parse_nodes(&input);
         assert_eq!(nodes.len(), 3);
-        assert_eq!(nodes[0].value, "AAA");
-        assert_eq!(nodes[0].left, "BBB");
-        assert_eq!(nodes[0].right, "CCC");
-        assert_eq!(nodes[1].value, "BBB");
-        assert_eq!(nodes[1].left, "AAA");
-        assert_eq!(nodes[1].right, "ZZZ");
-        assert_eq!(nodes[2].value, "ZZZ");
-        assert_eq!(nodes[2].left, "ZZZ");
-        assert_eq!(nodes[2].right, "ZZZ");
+        assert_eq!(nodes["AAA"][0], "BBB");
+        assert_eq!(nodes["AAA"][1], "CCC");
+        assert_eq!(nodes["BBB"][0], "AAA");
+        assert_eq!(nodes["BBB"][1], "ZZZ");
+        assert_eq!(nodes["ZZZ"][0], "ZZZ");
+        assert_eq!(nodes["ZZZ"][1], "ZZZ");
     }
 
     #[test]
@@ -89,5 +113,42 @@ mod tests {
         assert_eq!(instructions[2], Instruction::Right);
         assert_eq!(instructions[3], Instruction::Right);
         assert_eq!(instructions[4], Instruction::Left);
+    }
+
+    #[test]
+    fn test_walk_tree_6() {
+        let input = vec![
+            "AAA = (BBB, BBB)".to_string(),
+            "BBB = (AAA, ZZZ)".to_string(),
+            "ZZZ = (ZZZ, ZZZ)".to_string(),
+        ];
+        let nodes = parse_nodes(&input);
+        let instructions = parse_instructions("LLR");
+        let result = walk_tree(nodes, &instructions);
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_walk_tree_2() {
+        let input = vec![
+            "AAA = (BBB, CCC)".to_string(),
+            "BBB = (DDD, EEE)".to_string(),
+            "CCC = (ZZZ, GGG)".to_string(),
+            "DDD = (DDD, DDD)".to_string(),
+            "EEE = (EEE, EEE)".to_string(),
+            "GGG = (GGG, GGG)".to_string(),
+            "ZZZ = (ZZZ, ZZZ)".to_string(),
+        ];
+        let nodes = parse_nodes(&input);
+        assert_eq!(nodes.len(), 7);
+        let instructions = parse_instructions("RL");
+        let result = walk_tree(nodes, &instructions);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1("src/bin/day08/test_input_1.txt"), 2);
+        assert_eq!(part_1("src/bin/day08/test_input_2.txt"), 6);
     }
 }
