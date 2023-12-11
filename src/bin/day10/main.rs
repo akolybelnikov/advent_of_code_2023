c6abd1d1fc354f5dba1ae819813a72c2daeed99c
akolybelnikov <18685675+akolybelnikov@users.noreply.github.com>
2023-12-11 15:51:33 +0100

    day 11 parse and expand

--- src/bin/day10/main.rs
+++ src/bin/day10/main.rs
@@ -1,10 +1,12 @@
+use std::collections::HashSet;
+
 // --- Day 10: Pipe Maze ---
 type Coordinates = [i32; 2];
 
 #[derive(Default, Clone, Copy)]
 struct Tile {
-    entries: [Coordinates; 2],
-    is_ground: bool,
+    gates: [Coordinates; 2],
+    is_loop: bool,
     is_start: bool,
     position: Coordinates,
 }
@@ -12,8 +14,8 @@ struct Tile {
 impl Tile {
     fn new(entries: [Coordinates; 2], position: Coordinates) -> Tile {
         Tile {
-            entries,
-            is_ground: true,
+            gates: entries,
+            is_loop: false,
             is_start: false,
             position,
         }
@@ -21,39 +23,30 @@ impl Tile {
 
     fn start_tile(position: Coordinates) -> Tile {
         Tile {
-            entries: [[0; 2], [0; 2]],
-            is_ground: false,
+            gates: [[0; 2], [0; 2]],
+            is_loop: true,
             is_start: true,
             position,
         }
     }
 
-    fn ground_tile(position: Coordinates) -> Tile {
-        Tile {
-            entries: Default::default(),
-            is_ground: true,
-            is_start: false,
-            position,
-        }
-    }
     fn to(&self, from: Coordinates) -> Coordinates {
-        if self.entries[0] == from {
-            self.entries[1]
+        if self.gates[0] == from {
+            self.gates[1]
         } else {
-            self.entries[0]
+            self.gates[0]
         }
     }
 }
 
 #[derive(Default)]
 struct Maze {
-    ground: Vec<Tile>,
     start: Tile,
     tiles: Vec<Vec<Tile>>,
 }
 
 impl Maze {
-    fn find_start_entries(&mut self) {
+    fn find_start_gates(&mut self) {
         // Define the ranges around the start tile
         let x_range = [
             (self.start.position[0] - 1).max(0),
@@ -75,7 +68,7 @@ impl Maze {
                 }
                 // Check if the tile's list of entries contains the start position
                 if self.tiles[i as usize][j as usize]
-                    .entries
+                    .gates
                     .contains(&self.start.position)
                 {
                     entries.push([j, i]);
@@ -85,9 +78,45 @@ impl Maze {
 
         // Update start entries only if entries vec has enough items
         if entries.len() > 1 {
-            self.start.entries[0] = entries[0];
-            self.start.entries[1] = entries[1];
+            self.start.gates[0] = entries[0];
+            self.start.gates[1] = entries[1];
         }
+    }
+
+    fn find_flood_gates(&mut self) -> Vec<Coordinates> {
+        let mut entries: Vec<Coordinates> = Vec::new();
+        let y = self.start.position[1];
+        let x = self.start.position[0];
+        // tile to the north of start
+        if y > 0 {
+            let north = self.tiles[(y - 1) as usize][x as usize].position;
+            if !self.start.gates.contains(&north) {
+                entries.push(north);
+            }
+        }
+        // tile to the south of start
+        if y < (self.tiles.len() - 1) as i32 {
+            let south = self.tiles[(y + 1) as usize][x as usize].position;
+            if !self.start.gates.contains(&south) {
+                entries.push(south);
+            }
+        }
+        // tile to the east of start
+        if x < (self.tiles[0].len() - 1) as i32 {
+            let east = self.tiles[y as usize][(x + 1) as usize].position;
+            if !self.start.gates.contains(&east) {
+                entries.push(east);
+            }
+        }
+        // tile to the west of start
+        if x > 0 {
+            let west = self.tiles[y as usize][(x - 1) as usize].position;
+            if !self.start.gates.contains(&west) {
+                entries.push(west);
+            }
+        }
+
+        entries
     }
 
     fn walk_loop(
@@ -96,23 +125,58 @@ impl Maze {
         to: Coordinates,
         mut next: Coordinates,
     ) -> Vec<Coordinates> {
-        let mut positions = Vec::new();
-        positions.push(next);
+        let mut loop_tiles = Vec::new();
+        loop_tiles.push(next);
         let mut prev = from;
         while next != to {
             let tile = &mut self.tiles[next[1] as usize][next[0] as usize];
-            tile.is_ground = false;
+            tile.is_loop = true;
             let cur = next;
             next = tile.to(prev);
             prev = cur;
-            positions.push(next);
+            loop_tiles.push(next);
         }
-        positions
+        loop_tiles
+    }
+
+    fn fill_flood(&mut self, from: Coordinates) -> (HashSet<Coordinates>, bool) {
+        let mut visited: HashSet<Coordinates> = HashSet::new();
+        let offsets: [Coordinates; 4] = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+        let mut queue: Vec<Coordinates> = Vec::new();
+        queue.push(from);
+
+        while !queue.is_empty() {
+            let current = queue.pop().unwrap();
+            // if current is on the border, we're done
+            if current[0] == 0
+                || current[1] == 0
+                || current[0] == (self.tiles[0].len() - 1) as i32
+                || current[1] == (self.tiles.len() - 1) as i32
+            {
+                return (visited, false);
+            }
+            visited.insert(current);
+
+            for offset in offsets.iter() {
+                let next = [current[0] + offset[0], current[1] + offset[1]];
+
+                let is_valid = next[0] >= 0
+                    && next[1] >= 0
+                    && next[0] <= (self.tiles[0].len() - 1) as i32
+                    && next[1] <= (self.tiles.len() - 1) as i32;
+                if is_valid && !visited.contains(&next) {
+                    let tile = &self.tiles[next[1] as usize][next[0] as usize];
+                    if !tile.is_loop {
+                        queue.push(next);
+                    }
+                }
+            }
+        }
+        (visited, true)
     }
 }
 
 fn parse_input(input: &Vec<String>) -> Maze {
-    let ground_tiles: Vec<Tile> = Vec::new();
     let mut tiles: Vec<Vec<Tile>> = Vec::new();
     let mut maze = Maze::default();
     for (y, line) in input.iter().enumerate() {
@@ -123,7 +187,7 @@ fn parse_input(input: &Vec<String>) -> Maze {
                     [[x as i32 - 1, y as i32], [x as i32 + 1, y as i32]],
                     [x as i32, y as i32],
                 ),
-                '.' => Tile::ground_tile([x as i32, y as i32]),
+                '.' => Tile::new(Default::default(), [x as i32, y as i32]),
                 '7' => Tile::new(
                     [[x as i32, y as i32 + 1], [x as i32 - 1, y as i32]],
                     [x as i32, y as i32],
@@ -155,8 +219,7 @@ fn parse_input(input: &Vec<String>) -> Maze {
         tiles.push(row);
     }
     maze.tiles = tiles;
-    maze.ground = ground_tiles;
-    maze.find_start_entries();
+    maze.find_start_gates();
     maze
 }
 
@@ -166,9 +229,27 @@ fn part_1(filename: &str) -> i32 {
     let walk = maze.walk_loop(
         maze.start.position,
         maze.start.position,
-        maze.start.entries[0],
+        maze.start.gates[0],
     );
     walk.len() as i32 / 2
+}
+
+fn part_2(filename: &str) -> i32 {
+    let lines = advent_of_code_2023::read_lines(filename).unwrap();
+    let mut maze = parse_input(&lines);
+    maze.walk_loop(
+        maze.start.position,
+        maze.start.position,
+        maze.start.gates[0],
+    );
+    let flood_gates = maze.find_flood_gates();
+    let (flood, inner) = maze.fill_flood([0, 0]);
+    if inner {
+        flood.iter().count() as i32
+    } else {
+        let (flood, inner) = maze.fill_flood([2, 2]);
+        flood.iter().count() as i32
+    }
 }
 
 fn main() {
@@ -186,38 +267,56 @@ mod tests {
         assert_eq!(maze.tiles.len(), 5);
         assert_eq!(maze.tiles[0].len(), 5);
         assert_eq!(maze.start.position, [1, 1]);
-        assert!(maze.start.entries.contains(&[1, 2]));
-        assert!(maze.start.entries.contains(&[2, 1]));
+        assert!(maze.start.gates.contains(&[1, 2]));
+        assert!(maze.start.gates.contains(&[2, 1]));
     }
 
     #[test]
     fn test_walk() {
         let input = advent_of_code_2023::read_lines("src/bin/day10/test_input_1.txt").unwrap();
         let mut maze = parse_input(&input);
-        let positions = maze.walk_loop(
+        let loop_tiles = maze.walk_loop(
             maze.start.position,
             maze.start.position,
-            maze.start.entries[0],
+            maze.start.gates[0],
         );
-        assert_eq!(positions.len(), 8);
-        assert_eq!(positions[0], [2, 1]);
-        assert_eq!(positions[1], [3, 1]);
-        assert_eq!(positions[2], [3, 2]);
-        assert_eq!(positions[3], [3, 3]);
-        assert_eq!(positions[4], [2, 3]);
-        assert_eq!(positions[5], [1, 3]);
-        assert_eq!(positions[6], [1, 2]);
-        assert_eq!(positions[7], [1, 1]);
-        for position in positions {
-            assert!(!maze.tiles[position[1] as usize][position[0] as usize].is_ground);
+        assert_eq!(loop_tiles.len(), 8);
+        assert_eq!(loop_tiles[0], [2, 1]);
+        assert_eq!(loop_tiles[1], [3, 1]);
+        assert_eq!(loop_tiles[2], [3, 2]);
+        assert_eq!(loop_tiles[3], [3, 3]);
+        assert_eq!(loop_tiles[4], [2, 3]);
+        assert_eq!(loop_tiles[5], [1, 3]);
+        assert_eq!(loop_tiles[6], [1, 2]);
+        assert_eq!(loop_tiles[7], [1, 1]);
+        for position in loop_tiles {
+            assert!(maze.tiles[position[1] as usize][position[0] as usize].is_loop);
         }
-        let ground_tiles = maze.tiles.iter().flatten().filter(|t| t.is_ground).count();
-        assert_eq!(ground_tiles, 17);
     }
 
     #[test]
     fn test_part_1() {
         assert_eq!(part_1("src/bin/day10/test_input_1.txt"), 4);
         assert_eq!(part_1("src/bin/day10/test_input_2.txt"), 8);
+    }
+
+    #[test]
+    fn test_fill_flood() {
+        let input = advent_of_code_2023::read_lines("src/bin/day10/test_input_3.txt").unwrap();
+        let mut maze = parse_input(&input);
+        let _loop_tiles = maze.walk_loop(
+            maze.start.position,
+            maze.start.position,
+            maze.start.gates[0],
+        );
+        let flood = maze.fill_flood([1, 0]);
+        assert!(!flood.1);
+    }
+
+    #[test]
+    fn test_part_2() {
+        //assert_eq!(part_2("src/bin/day10/test_input_1.txt"), 4);
+        // assert_eq!(part_2("src/bin/day10/test_input_2.txt"), 8);
+        assert_eq!(part_2("src/bin/day10/test_input_3.txt"), 24);
     }
 }
--- src/bin/day11/main.rs
+++ src/bin/day11/main.rs
@@ -1,3 +1,164 @@
+// --- Day 11: Cosmic Expansion ---
+
+#[derive(Debug, PartialEq)]
+enum CellType {
+    Empty,
+    Galaxy,
+}
+
+type Coordinates = (i32, i32);
+
+struct Cell {
+    cell_type: CellType,
+    coordinates: Coordinates,
+}
+
+impl Cell {
+    fn new(cell_type: CellType, coordinates: Coordinates) -> Cell {
+        Cell {
+            cell_type,
+            coordinates,
+        }
+    }
+}
+
+struct Image {
+    cells: Vec<Vec<Cell>>,
+}
+
+impl Image {
+    fn new() -> Image {
+        Image { cells: Vec::new() }
+    }
+
+    fn row_is_empty(&self, idx: usize) -> bool {
+        self.cells[idx]
+            .iter()
+            .all(|cell| cell.cell_type == CellType::Empty)
+    }
+
+    fn column_is_empty(&self, idx: usize) -> bool {
+        self.cells
+            .iter()
+            .all(|row| row[idx].cell_type == CellType::Empty)
+    }
+
+    fn insert_empty_row(&mut self, idx: usize) {
+        let mut row = Vec::new();
+        for x in 0..self.cells[0].len() {
+            row.push(Cell::new(CellType::Empty, (x as i32, idx as i32)));
+        }
+        self.cells.insert(idx, row);
+    }
+
+    fn insert_empty_column(&mut self, idx: usize) {
+        for (y, row) in self.cells.iter_mut().enumerate() {
+            row.insert(idx, Cell::new(CellType::Empty, (idx as i32, y as i32)));
+        }
+    }
+
+    fn expand_rows(&mut self) {
+        let empty_rows = self
+            .cells
+            .iter()
+            .enumerate()
+            .filter(|(idx, _)| self.row_is_empty(*idx))
+            .map(|(idx, _)| idx)
+            .collect::<Vec<usize>>();
+        let mut count = 0;
+        for y in empty_rows {
+            self.insert_empty_row(y + 1 + count);
+            count += 1;
+        }
+    }
+
+    fn expand_columns(&mut self) {
+        let empty_columns = self
+            .cells[0]
+            .iter()
+            .enumerate()
+            .filter(|(idx, _)| self.column_is_empty(*idx))
+            .map(|(idx, _)| idx)
+            .collect::<Vec<usize>>();
+        let mut count = 0;
+        for x in empty_columns {
+            self.insert_empty_column(x + 1 + count);
+            count += 1;
+        }
+    }
+}
+
+fn parse_image(lines: Vec<String>) -> Image {
+    let mut image = Image::new();
+
+    for (y, line) in lines.iter().enumerate() {
+        let mut row = Vec::new();
+        for (x, c) in line.chars().enumerate() {
+            let cell_type = match c {
+                '.' => CellType::Empty,
+                '#' => CellType::Galaxy,
+                _ => panic!("Unknown cell type: {}", c),
+            };
+            row.push(Cell::new(cell_type, (x as i32, y as i32)));
+        }
+        image.cells.push(row);
+    }
+
+    image
+}
+
 fn main() {
     println!("Hello from day11!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_image() {
+        let lines = advent_of_code_2023::read_lines("src/bin/day11/test_input.txt").unwrap();
+        let image = parse_image(lines);
+        assert_eq!(image.cells.len(), 10);
+        assert_eq!(image.cells[0].len(), 10);
+        assert_eq!(image.cells[0][0].cell_type, CellType::Empty);
+        assert_eq!(image.cells[0][0].coordinates, (0, 0));
+        assert_eq!(image.cells[0][3].cell_type, CellType::Galaxy);
+        assert_eq!(image.cells[0][3].coordinates, (3, 0));
+        assert_eq!(image.cells[9][0].cell_type, CellType::Galaxy);
+        assert_eq!(image.cells[9][0].coordinates, (0, 9));
+    }
+
+    #[test]
+    fn test_expand_rows() {
+        let lines = advent_of_code_2023::read_lines("src/bin/day11/test_input.txt").unwrap();
+        let mut image = parse_image(lines);
+        assert_eq!(image.cells.len(), 10);
+        assert!(image.row_is_empty(3));
+        assert!(image.row_is_empty(7));
+        image.expand_rows();
+        assert_eq!(image.cells.len(), 12);
+        assert!(image.row_is_empty(3));
+        assert!(image.row_is_empty(4));
+        assert!(image.row_is_empty(8));
+        assert!(image.row_is_empty(9));
+    }
+
+    #[test]
+    fn test_expand_columns() {
+        let lines = advent_of_code_2023::read_lines("src/bin/day11/test_input.txt").unwrap();
+        let mut image = parse_image(lines);
+        assert_eq!(image.cells[0].len(), 10);
+        assert!(image.column_is_empty(2));
+        assert!(image.column_is_empty(5));
+        assert!(image.column_is_empty(8));
+        image.expand_columns();
+        assert_eq!(image.cells[0].len(), 13);
+        assert!(image.column_is_empty(2));
+        assert!(image.column_is_empty(3));
+        assert!(image.column_is_empty(6));
+        assert!(image.column_is_empty(7));
+        assert!(image.column_is_empty(10));
+        assert!(image.column_is_empty(11));
+    }
 }
